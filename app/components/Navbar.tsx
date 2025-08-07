0d209d241a40989deff28d228d67149a126db41b
Eduardo Alves <edu0alves00@gmail.com>
2025-08-06 23:40:39 -0300

    feat: homepage, navbar and resume card

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <Link to="/">
+        <p className="text-2xl font-bold text-gradient">RESUMIND</p>
+      </Link>
+      <Link to="/upload" className="primary-button w-fit">
+        Envie seu currículo
+      </Link>
+    </nav>
+  );
+};
+
+export default Navbar;
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,40 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    {
+      name: "description",
+      content: "Feedback inteligente para o seu trabalho dos sonhos!",
+    },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading">
+          <h1>Acompanhe suas inscrições & Avaliações de currículos </h1>
+          <h2>
+            Revise seus envios e tenha feedback com Inteligência Artificial
+          </h2>
+        </div>
+
+        {resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume) => (
+              <div>
+                <ResumeCard key={resume.id} resume={resume} />
+              </div>
+            ))}
+          </div>
+        )}
+      </section>
+    </main>
+  );
 }
